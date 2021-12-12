acb449e4a55800584c5cfea3ab3342b8d3728460
Marco Kang <kmnkit@gmail.com>
2021-12-12 22:34:45 +0900

    명언 제조 앱 / 名言製造アプリ

---
+++ QuotesGenerator/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  QuotesGenerator
+//
+//  Created by Sungwoong Kang on 2021/12/12.
+//
+
+import Foundation
+
+struct Quote {
+    let contents: String
+    let name: String
+}
